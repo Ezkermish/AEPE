e9b37b47601dd7494f7b218ad68c50ae795e1978
Carlos Quiroz <1943778+Ezkermish@users.noreply.github.com>
2023-05-09 15:16:59 -0600

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,6 @@
+<?php
+    $dbhost	= "localhost";	// localhost o IP
+    $dbuser	= "root";		// usuario de la BD
+    $dbpass	= "";	        // pass de la BD
+    $dbname	= "epe_pelo";   // nombre de la BD
+?>
